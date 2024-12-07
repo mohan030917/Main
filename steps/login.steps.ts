47fa87a8fd95586cdc9d4a236dca422d95b8a857
mohan123 <mohan030917@gmail.com>
2024-12-07 16:05:29 +0530

    changes

--- steps/login.steps.ts
+++ steps/login.steps.ts
@@ -13,7 +13,7 @@ Given('I am on the Product Store page', async ()=> {
         await page.locator("//div[@class='bwc__sc-1ihur1g-5 otNAQ in-animation']//form//div//label[@for='mobileNo']").pressSequentially("9840678772")
         await page.waitForTimeout(1000);
         await page.locator("//div[@class='bwc__sc-1ihur1g-5 otNAQ in-animation']//button").click();
-        await page.waitForTimeout(3000);
+        await page.waitForTimeout(3000)
         
     } catch (error) {
         throw error
